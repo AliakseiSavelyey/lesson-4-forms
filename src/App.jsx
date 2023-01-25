77a1fdabe650021765d83bc56c575d182f4b7e00
Алексей Савельев <mtptz21@gmail.com>
2023-01-25 21:40:12 +0300

    feat: add new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Notification message="There is no feedback"></Notification>
+      <Section title="">
+        <FeedbackOptions options={} onLeaveFeedback={}></FeedbackOptions>
+      </Section>
+
+      <Section title="">
+              <Statistics good={} neutral={} bad={} total={} positivePercentage={}></Statistics>
+    </Section>
     </div>
   );
 };
